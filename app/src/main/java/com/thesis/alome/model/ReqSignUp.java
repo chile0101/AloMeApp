c70db5acfbeb9c47ccfcc45d312dfbd8ac0c6722
lexuanchinh97 <lexuanchinh1507@gmail.com>
2019-11-02 19:42:07 +0700

    updated signup

--- app/src/main/java/com/thesis/alome/model/ReqSignUp.java
+++ app/src/main/java/com/thesis/alome/model/ReqSignUp.java
@@ -4,10 +4,12 @@ public class ReqSignUp{
     private String username;
     private String password;
     private String fullName;
+    private String phone;
 
-    public ReqSignUp(String username, String password, String fullName) {
+    public ReqSignUp(String username, String password, String fullName, String phone) {
         this.username = username;
         this.password = password;
         this.fullName = fullName;
+        this.phone = phone;
     }
 }
